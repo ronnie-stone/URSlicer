ad658e506b157de82af69b9469841eb77548ec4e
John Lyle <johnlyleiv@gmail.com>
2025-02-24 13:54:15 -0600

    file add

---
+++ slicer_library/ur_slicer.py
@@ -0,0 +1,23 @@
+from test_config import config
+import numpy
+from stl import mesh
+
+class STL2Motion():
+    """Fill in comment"""
+    def __init__(self, filepath_to_stl, quaternion):
+        self.filepath = filepath_to_stl
+        self.input_quaternion = quaternion       
+    def create_motion_path():
+        """Fill in comment"""
+        # Example function
+        pass
+
+def main():
+    """Fill in comment"""
+    # Write any testing code here, will be executed if the file is ran directly. Usually the file will be used by importing class
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ ur_slicer_io_control/ur_slicer_io_control/io_control_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ur_slicer_io_control.')
+
+
+if __name__ == '__main__':
+    main()
