b5309a1c57225b45890652ba019ca409fc7b7004
John Lyle <johnlyleiv@gmail.com>
2025-04-05 15:28:52 -0500

    fixed build dependencies to allow for pure usage of colcon build

--- ur_slicer_io_control/setup.py
+++ ur_slicer_io_control/setup.py
@@ -1,26 +1,29 @@
 from setuptools import find_packages, setup
 
-package_name = 'ur_slicer_io_control'
+package_name = "ur_slicer_io_control"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        "setuptools",
+        "rclpy",
+        "ur_slicer_interfaces",
+    ],
     zip_safe=True,
-    maintainer='john',
-    maintainer_email='johnlyleiv@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="john",
+    maintainer_email="johnlyleiv@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'io_control_node = ur_slicer_io_control.io_control_node:main'
+        "console_scripts": [
+            "io_control_node = ur_slicer_io_control.io_control_node:main"
         ],
     },
 )
